7a63e44f7462bbe100a166e14545bd5df5a2e07d
”名前” <>
2019-11-07 02:20:22 +0000

    PHP/Laravel 10 ControllerとViewが連携できるようにしようの課題です

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>create.blade.php課題</title>
+    </head>
+    <body>
+        <h1>create.blade.php</h1>
+        <p>create.blade.phpの課題です</p>
+    </body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>edit.blade.php課題</title>
+    </head>
+    <body>
+        <h1>edit.blade.php</h1>
+        <p>edit.blade.phpの課題です</p>
+    </body>
+</html>
