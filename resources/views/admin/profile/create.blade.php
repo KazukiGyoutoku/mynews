44764d282faaeecd2514b2235abcf013221a4e40
”名前” <>
2019-11-13 01:31:37 +0000

    PHP/Laravel 11 Bladeテンプレートの継承とLaravel Mixの使い方を活用してみようの課題です

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,11 +1,14 @@
-<!DOCTYPE>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>create.blade.php課題</title>
-    </head>
-    <body>
-        <h1>create.blade.php</h1>
-        <p>create.blade.phpの課題です</p>
-    </body>
-</html>
+@extends('layouts.admin')
+ 
+ @section('title','Re:LieF~親愛なるあなたへ~')
+  
+  @section('content')
+  <div class ="container">
+    <div class="row">
+        <div class ="col-md-8 mx-auto">
+            <h2>キャッチコピー,もう一度学園生活を送ってみませんか</h2>
+        </div>
+   </div>
+ </div>
+  @endsection
+    
